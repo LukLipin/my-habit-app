80347eef7a8cc281af26def566181082490cc672
luklipin <l.k.lipinski@gmail.com>
2025-10-14 13:58:48 +0200

    First push

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+function App() {
+  return (
+      <div>
+       
+      </div>
+  )
+}
+
+export default App
+
+
+function Header (){
+  return (
+    <header>
+      <h1> </h1>
+    </header>
+  )
+}
